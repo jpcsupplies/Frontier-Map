b283af40031fa2fb96660384fb52cc7d447a6be2
xPhoenixXx <jpcsupplies@hotmail.com>
2018-04-23 17:05:50 +1000

    reformatting for internal readability
    Getting ready to implement the master index file so have
    re-formatted the internal layout of the perl script

--- Webserver/updatemap.pl
+++ Webserver/updatemap.pl
@@ -2,10 +2,10 @@
 use strict;
 use CGI qw(:standard);
 
-#This is a prototype web map editor
-#not particularly secure, but should work.
+#This is a prototype web map aggregator
+#not particularly secure, but should work to populate online map
 #takes data sent from client, updates index file, and map data for 
-#client sector
+#client sectors categorised by "network"
 #id needs to revert to 0 if invalid
 #index file should include key (unencrypted), network, grid ID, IP, filename, last updated timestamp ?
 #when getting data -
@@ -19,6 +19,10 @@ use CGI qw(:standard);
 # job=something to write, nothing to display only. Default.
 
 
+##################################
+#Init major variables and settings
+##################################
+
 my $mode=param('mode');
 my $network=param('network');
 my $ID=param('ID');
@@ -28,17 +32,56 @@ my $ownpage='http://' . "$ENV{SERVER_NAME}$ENV{SCRIPT_NAME}"; #cgi script runnin
 my $actualpage=$ENV{HTTP_REFERER}; #referer(); 
 my $IP=$ENV{REMOTE_ADDR};
 my $tdata=" ";
-my $page="text";
+my $page="text"; my $indexfile="mapindex.csv";
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
+my $timestamp = sprintf("%4d-%02d-%02d %02d:%02d:%02d ",$year+1900,$mon+1,$mday,$hour,$min,$sec);
 
-# print our header.
+
+#########################
+# print our html header.
+#########################
+
 print header();
 print '<html>';
+
+
+##########################################
+# detect or assign current operating mode
+##########################################
 
 if (!$mode) { $mode = "read"; }
 if (!$MapData) { $mode = "read"; }
 
 
+
+######################
+#notes and sample data
+######################
+#remove end of line OS garbage
+#$setup_data =~ s/\r|\n//g;        
+#my $net_name=$_[0];
+#sample data
+#Ramblers Frontier C | Mars Sector | 0.0.0.0:27015 | Ramblers Frontier - Mars Sector. Corporate HQ of the Frontier Trade Network,,,,,,,,,
+#Type,Name,X,Y,Z,Size,Speed,VectorX,VectorY,VectorZ
+
+
+#########################
+#Check or Init Data Files
+#########################
+
+#Lazy way to check file, append a nul to file. This creates if not existing
+open(TDATA, ">>$page")  or die "Debug File Error $!";
+	print TDATA "";
+close(TDATA);
+
+open(TDATA, ">>$indexfile")  or die "Index File Error $!";
+	print TDATA "";
+close(TDATA);
+
+
+##################################################
+#generate debug output and populate mapinfo fields
+##################################################
 print "<b>Debugging 0 info:</b><br><pre>
 [$IP] via [$actualpage]. 
 Mode requested: [$mode]. 
@@ -48,56 +91,17 @@ Path translated: [$ENV{PATH_TRANSLATED}]
 Server: [$ENV{SERVER_NAME}] 
 Script: [$ENV{SCRIPT_NAME}]  
 Cgi request: [$ENV{QUERY_STRING}]
-My Script: [$ownpage]</pre>";
-
-###########
-#need to get out the server port and bond it with the actual IP we got
-#split line 1 by , and :, then $ip . $port for steam connect address?
-#my $server_address="0.0.0.0";
-
-#remove end of line OS garbage
-#$setup_data =~ s/\r|\n//g;
-               
-#my $net_name=$_[0];
-#sample data
-#Ramblers Frontier C | Mars Sector | 0.0.0.0:27015 | Ramblers Frontier - Mars Sector. Corporate HQ of the Frontier Trade Network,,,,,,,,,
-#Type,Name,X,Y,Z,Size,Speed,VectorX,VectorY,VectorZ
-
-###########
-
-
-
-#Lazy way to check file, append a nul to file. This creates if not existing
-open(TDATA, ">>$page")  or die "File Error $!";
-	print TDATA "";
-close(TDATA);
-
-my $timestamp = sprintf("%4d-%02d-%02d %02d:%02d:%02d ",$year+1900,$mon+1,$mday,$hour,$min,$sec);
-
+My Script: [$ownpage]
+Debug file: [$page]</pre>";
 print $timestamp;
 
-#debugging only here actual data should be confirmed and written to appropriate map index file.
-
-#generate debug output (not used by map just for checking output)
 if ($mode eq "update") { 
-open(TDATA, ">$page")  or die "File Error $!"; 
-        my ($servername, $mapname, $ipport, $description, $mapping)=split(/\|/,$MapData,5); #seperate first 4 fields by | deliminer assign everything else to $mapping#
-        my ($localip,$port)=split(/:/,$ipport,2);
-        my $steamconnect=join(':',$IP,$port);
-        $steamconnect=~ s/ //g; #remove whitepaces
-	print TDATA "Last Update: $timestamp\nGameserver: [$steamconnect] \nKey [$key]\nNetwork: $network \nRequests Grid: $ID \nServername: $servername \nMapname: $mapname \nIPPORT: $ipport \nDescription: $description\n";
-	#print TDATA "\nData Dump: \n$MapData\n";
-	print TDATA "\nMap Dump: \n$mapping\n";
-	print TDATA "\n";
- close(TDATA); 
- 
- my $livefile="Live.csv";
- open(TDATA, ">$livefile")  or die "File Error $!"; 
-	print TDATA $MapData;
-	print TDATA "\n";
- close(TDATA); 
- 
- }
+# open index file and load into memory, close index file
+
+# check if supplied data matches existing data or if we need to add it ?
+
+#network, grid, server details ip name etc, key, filename
+#network will populate galaxy list selection box?
 
 #what we should be doing here is checking index file grid $ID to see if $key matches THEN updating the appropriate map csv file
 #if it is a new server in the group and key is valid attempt to allocate desired Grid id, otherwise (fail) ?  or should i tell pushmap back to update their ID?
@@ -114,6 +118,30 @@ open(TDATA, ">$page")  or die "File Error $!";
 #data validation if > 1000, if <0, (ignore) AND we also need to stop people trying to fly off into nulspace eg if someone at the right most side (10/20/30/40/50/60/70/80/90/100) tries to go right (ignore)
 #if someone at the back (1-10) tries to go backwards (ignore)  if someone at the front (91-100) tries to go forwards (ignore) if someone on the left (01/11/21/31/41/51/61/71/81/91) (ignore)
 
+open(TDATA, ">$page")  or die "File Error $!"; 
+	#seperate first 4 fields by | deliminer assign everything else to $mapping#
+        my ($servername, $mapname, $ipport, $description, $mapping)=split(/\|/,$MapData,5); 
+        my ($localip,$port)=split(/:/,$ipport,2);
+        my $steamconnect=join(':',$IP,$port);
+        $steamconnect=~ s/ //g; #remove whitepaces
+	print TDATA "Last Update: $timestamp\nGameserver: [$steamconnect] \nKey [$key]\nNetwork: $network \nRequests Grid: $ID \nServername: $servername \nMapname: $mapname \nIPPORT: $ipport \nDescription: $description\n";
+	#print TDATA "\nData Dump: \n$MapData\n";
+	print TDATA "\nMap Dump: \n$mapping\n";
+	print TDATA "\n";
+ close(TDATA); 
+ 
+ #here we actually write to the live map#
+ my $livefile="Live.csv";
+ open(TDATA, ">$livefile")  or die "File Error $!"; 
+	print TDATA $MapData;
+	print TDATA "\n";
+ close(TDATA); 
+}
+
+
+#############################################
+# Debug - display last map dump data received
+#############################################
 print '<p>Messages<br><hr><textarea rows="50" cols="120">';
 open(TDATA, $page) or die "File Error $!";
 while (<TDATA>) {
@@ -123,7 +151,10 @@ while (<TDATA>) {
 close(TDATA);
 print '</textarea></p><hr>End.';
 
+
+#############################
 # end of script end the html
+#############################
 print end_html;
 
 
